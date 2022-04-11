90f6770c59d3fb0ec787ecabe7f388c14d68e80c
Houdaïfa Mofril <houdaifadevelopper@gmail.com>
2022-04-11 07:29:43 +0200

    Ajout projets et liens sur le fichier projets.js

--- src/pages/projets.js
+++ src/pages/projets.js
@@ -14,8 +14,13 @@ const ProjetsPage = () => {
             <Helmet title="Projets" />
             <h1>Projets</h1>
             <ul css={{listStyleType:"none",}} >
+                <li><ProjetsLink to="https://github.com/HoudaifaMofril/personal-site-v2/" >١{" "}houdaifamofril.com : Mon Site Personnel</ProjetsLink></li>
+                <li><ProjetsLink to="https://github.com/HoudaifaMofril/HoudaifaMofril_7_26062021/" >١{" "}Groupomania: Réseau social d'entreprise</ProjetsLink></li>
+                <li><ProjetsLink to="https://github.com/HoudaifaMofril/HoudaifaMofril_6_25_05_2021/" >١{" "}SoPekocko: Application de partage de sauces piquantes</ProjetsLink></li>
+                <li><ProjetsLink to="https://github.com/HoudaifaMofril/HoudaifaMofril_5_22032021/" >١{" "}Orinoco: Site e-commerce</ProjetsLink></li>
                 <li><ProjetsLink to="https://houdaifamofril.github.io/HoudaifaMofril_3_19012021/" >١{" "}Oh my Food !</ProjetsLink></li>
                 <li><ProjetsLink to="https://houdaifamofril.github.io/HoudaifaMofril_2_17122020/" >١{" "}Reservia: Site d'hébergement</ProjetsLink></li>
+
 
             </ul>
 
